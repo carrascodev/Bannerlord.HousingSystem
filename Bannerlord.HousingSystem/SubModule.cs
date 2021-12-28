038ec0ceb80e70df8dfc779b936db67b00797ade
Henrique Pagnossi <henrique.carrasco.1@gmail.com>
2021-12-28 17:30:00 -0300

    first

---
+++ Bannerlord.HousingSystem/SubModule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using TaleWorlds.Core;
+using TaleWorlds.Engine.GauntletUI;
+using TaleWorlds.Engine.Screens;
+using TaleWorlds.GauntletUI;
+using TaleWorlds.Library;
+using TaleWorlds.MountAndBlade;
+using TaleWorlds.MountAndBlade.View.Missions;
+
+namespace Bannerlord.HousingSystem;
+
+public class SubModule : MBSubModuleBase
+{
+    protected override void OnBeforeInitialModuleScreenSetAsRoot()
+    {
+        base.OnBeforeInitialModuleScreenSetAsRoot();
+        InformationManager.DisplayMessage(new InformationMessage(
+            "HousingSystem loaded!!",
+            Color.FromUint(Convert.ToUInt32("0x92c3fb", 16))));
+    }
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# About
+This mod add a player housing feature to Bannerlord.
+
+## Features Breakdown
+
+### Planned
+* Housing Menu in towns
+  * Buy
+  * Rent
+  * Enter
+  * Open Household Stash
+* House Scenario
+  * Open Stash
